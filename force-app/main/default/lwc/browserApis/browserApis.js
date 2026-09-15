b8aa89e60eabc2e93391d069349e19f09e50ec0b
Harini Govindarajan <83868381+Harini005@users.noreply.github.com>
2026-09-15 21:24:58 +0530

    Uncomment browser API handler methods
    Restored alert, confirm, and prompt handlers for user interactions.

--- force-app/main/default/lwc/browserApis/browserApis.js
+++ force-app/main/default/lwc/browserApis/browserApis.js
@@ -1,18 +1,16 @@
 import { LightningElement } from "lwc";
 
 export default class BrowserApis extends LightningElement {
-  /**
-   *  These has to be commented for the git repository purpose. In the org these works absolutely fine.
-   */
-  // alertHandler(){
-  //     alert('This is the alert message');
-  // }
-  // async confirmHandler(){
-  //    let resp =  confirm('Are you ok with the changes');
-  //    console.log(resp);
-  // }
-  // promptHandler(){
-  //    let resp = prompt('Please Enter the order N0');
-  //    console.log(resp);
-  // }
+  
+  alertHandler(){
+      alert('This is the alert message');
+  }
+  async confirmHandler(){
+     let resp =  confirm('Are you ok with the changes');
+     console.log(resp);
+  }
+  promptHandler(){
+     let resp = prompt('Please Enter the order N0');
+     console.log(resp);
+  }
 }
